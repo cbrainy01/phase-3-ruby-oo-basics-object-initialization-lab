e63e0f7a22f947e44239d689a50537612cb630cc
Carl <carlbrainegebe@gmail.com>
2021-07-19 22:44:40 -0500

    commit tree 9897864d6c00b61e276e50a2443c86afd814652e

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+    def initialize(name, breed = "Mutt")
+        @name = name
+        @breed = breed
+    end
+end
+
+
+
+# class Player
+#     def meas
+#         measurables = {
+#             height: @height,
+#             weight: @weight,
+#         }
+#     end
+#     def initialize(height, weight)
+#         @height = height
+#         @weight = weight
+#     end
+# end
+
+# kyler = Player.new("5'10", 207)
+
+# kyler.meas
